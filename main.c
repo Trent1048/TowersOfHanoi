107a559b87a08d1e09bac0dda03cdcbe3b94eb85
Trent1048 <trentbultsma@gmail.com>
2020-03-04 13:40:14 -0800

    make the code not have so many nasty pointers

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "stdio.h"
-const int size = 3;
+const int SIZE = 4;
 
-void printGame(int game[3][size])
+void printGame(int game[3][SIZE])
 {
-	for(int i = 0; i < size; i++)
+	for(int i = 0; i < SIZE; i++)
 	{
-		printf( "[%d] [%d] [%d]\n", game[0][size-1-i], game[1][size-1-i], game[2][size-1-i]);
+		printf("[%d] [%d] [%d]\n", game[0][SIZE-1-i], game[1][SIZE-1-i], game[2][SIZE-1-i]);
 	}
 }
 
@@ -26,24 +26,27 @@ int getSpare(int src, int trg)
 	else return -1;
 }
 
-int lowest(int (*board)[size], int peg)
+// returns the lowest space available on a peg
+// returns -1 if the peg is full
+int getSpace(int (*board)[3][SIZE], int peg)
 {
-	for(int i = 0; i < size; i++)
+	//int realGame[3][SIZE] = &board;
+	for(int i = 0; i < SIZE; i++)
 	{
-		int val = *( *(board + peg) + i);
+		int val = (*board)[peg][i];
 		if(val==0) return i;
 	}
 	return -1;
 }
 
-void move(int (*board)[size], int bDisk, int tDisk, int src, int trg)
+void move(int (*board)[3][SIZE], int bDisk, int tDisk, int src, int trg)
 {
 	int spa = getSpare(src, trg);
     if(tDisk==bDisk)
     {
-        int save = *( *(board + src) + bDisk);
-        *( *(board + src) + bDisk) = 0;
-        *( *(board + trg) + lowest(board, trg)) = save;
+		int* saveLoc = &(*board)[src][bDisk];
+		(*board)[trg][getSpace(board, trg)] = *saveLoc;
+		*saveLoc = 0;
     }
     else
     {
@@ -58,18 +61,17 @@ void move(int (*board)[size], int bDisk, int tDisk, int src, int trg)
 
 int main(void) 
 {
-	int game[3][size];
+	int game[3][SIZE];
 	for(int i = 0; i < 3; i++)
 	{
-		for(int j = 0; j < size; j++)
+		for(int j = 0; j < SIZE; j++)
 		{
-			if(i==0) game[i][j] = size-j;
+			if(i==0) game[i][j] = SIZE-j;
 			else game[i][j] = 0;
 		}
 	}
-	int (*gamep)[size];
-	gamep = game;
-	move(gamep, 0, size-1, 0, 2);
+	move(&game, SIZE-1, SIZE-1, 0, 2);
+	printGame(game);
 }
 
 
@@ -84,13 +86,14 @@ int main(void)
 
 
 /*
+// old code for reference
 void move(int bDisk, int tDisk, int src, int trg)
 {
     if(tDisk==bDisk)
     {
         int save = game[src][bDisk];
         game[src][bDisk] = 0;
-        game[trg][lowest(game, trg)] = save;
+        game[trg][getSpace(game, trg)] = save;
     }
     else
     {
